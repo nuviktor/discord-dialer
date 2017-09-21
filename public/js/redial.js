22367211f7ff17e00fae8a8c59a00268a1096bae
Viktor Numerov <nuviktor@yahoo.com>
2017-09-21 17:36:31 +0100

    Added missing reference to redial.js and getRandomInt function after mix up with commits

--- public/js/redial.js
+++ public/js/redial.js
@@ -1,3 +1,9 @@
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 function parseRedialSpec(spec) {
   var redialObject = [];
 
